305c17c0dfb0eb69498f05c46872d05e0d83b6bb
surnan <forsuresh@yandex.com>
2019-01-14 16:26:58 -0500

    minor code cleanup

--- dogAPI_Test/dogAPI_Test/Model/DogAPI.swift
+++ dogAPI_Test/dogAPI_Test/Model/DogAPI.swift
@@ -11,11 +11,7 @@ import Foundation
 class DogAPI {
     
     enum Endpoint: String {
-        case randomImageFromAllDogsCollection = "https://dog.ceo/api/breeds/image/random"
-        
-        case randomImageFromAllDogsCollection2 =  "https://images.dog.ceo/breeds/appenzeller/n02107908_3613.jpg"
-        
-        
+        case randomImageFromAllDogsCollection = "https://dog.ceo/api/breeds/image/random"  //link to JSON
         
         var url: URL {
             return URL(string: self.rawValue)! //force-unwrapping will be safe here.  It's an enum
