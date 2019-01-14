02902353c5ea75406352017e80d92d669873514a
surnan <forsuresh@yandex.com>
2019-01-13 22:36:58 -0500

    Project Created

---
+++ dogAPI_Test/dogAPI_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dogAPI_Test
+//
+//  Created by admin on 1/13/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
